03b50f9d55c5b4b2a3005e565e8744db4411d297
jairomelo <jairoantoniomelo@gmail.com>
2026-03-23 12:12:03 -0700

    Add artifact location extraction and improve data consolidation logic

--- code/data.py
+++ code/data.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import os
 import uuid
+from pathlib import Path
+from glob import glob
 
 def consolidate_data(data_folder, output_folder=None, output_file="consolidated_data.csv", dry_run=False):
     
@@ -21,13 +23,29 @@ def consolidate_data(data_folder, output_folder=None, output_file="consolidated_
     consolidated_df = consolidated_df.replace(r'\s', ' ', regex=True)
     
     consolidated_df['objectid'] = consolidated_df.apply(lambda _: str(uuid.uuid4()), axis=1)
+    consolidated_df['artifacts_location'] = consolidated_df['filename'].apply(lambda x: _locate_artifacts(x, "artifacts", None))
     
     if not dry_run:
         consolidated_df.to_csv(os.path.join(output_folder, output_file), index=False)
         print(f"Consolidated data saved to {output_file}")
     else:
         print(f"Dry run: Consolidated data would be saved to {output_file} with {len(consolidated_df)} rows.")
-        print(consolidated_df.head())
+        print(consolidated_df['artifacts_location'].head())
+
+def _locate_artifacts(path, origin, patterns):
+    name, extension = os.path.splitext(path)
+    if "Fotos" in name:
+        origin = os.path.join(origin, "EuroPosts")
+    elif "C2-G2" in name:
+        origin = os.path.join(origin, "sitiopostales")
+    else:
+        origin = os.path.join(origin, "Coleccion-MC")
+    
+    search_pattern = os.path.join(origin, f"{Path(path).stem}-*.*")
+    # print(f"Searching for files matching: {search_pattern}")
+    matching_files = glob(search_pattern)
+    return matching_files
+
 
 if __name__ == "__main__":
-    consolidate_data("data")
+    consolidate_data(data_folder="data", output_folder="data/processed", output_file="consolidated_data.csv", dry_run=False)
